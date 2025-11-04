a1b01d829de96fbdc02cc969e34a53a7c19688c1
Ross Crawford <ross.hunter.crawford@gmail.com>
2025-11-04 16:10:18 +1300

    Backend implementation: Added Models, Controllers, DbContext, Migrations, and Swagger configuration

---
+++ VMS.WebApp/VMS.WebApp/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿namespace VMS.WebApp.Models
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public int VisitorID { get; set; }
+        public int EventID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickets { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // Navigation properties - creates relationships
+        public Visitor Visitor { get; set; } = null!;
+        public Event Event { get; set; } = null!;
+    }
+}
