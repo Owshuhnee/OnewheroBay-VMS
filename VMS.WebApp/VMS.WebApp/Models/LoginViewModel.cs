38cc7c5d4a0b62758d85db035c34d3bb7e38334f
Jove Tondo <jove.tondo@gmail.com>
2025-11-09 23:21:47 +1300

    Added Login function

---
+++ VMS.WebApp/VMS.WebApp/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace VMS.WebApp.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
