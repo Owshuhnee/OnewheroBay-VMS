f1a1730027616a25146f35d6cb1cc70fdb0fd984
Jove Tondo <jove.tondo@gmail.com>
2025-11-09 22:39:07 +1300

    Added Registration flow and connection to Neon PostgreSQL

---
+++ VMS.WebApp/VMS.WebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VMS.WebApp.Models
+{
+    public class RegisterViewModel
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ VMS.WebApp/VMS.WebApp/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace VMS.WebApp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }         
+        public string Role { get; set; } = "Visitor";
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!; 
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
