d7cfb734b5a1fd546f85122b6af636b05c554f76
Ross Crawford <ross.hunter.crawford@gmail.com>
2025-11-10 22:06:00 +1300

    Resolve merge conflicts - use API controller version

--- VMS.WebApp/VMS.WebApp/Controllers/BookingsController.cs
+++ VMS.WebApp/VMS.WebApp/Controllers/BookingsController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VMS.WebApp.Data;
 using VMS.WebApp.Models;
@@ -11,21 +10,6 @@ namespace VMS.WebApp.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class BookingsController : ControllerBase
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using VMS.WebApp.Data;
-using VMS.WebApp.Models;
-
-namespace VMS.WebApp.Controllers
-{
-    public class BookingsController : Controller
->>>>>>> aaa622ac58bfdcc7921fbe7a32ff5642379546f7
     {
         private readonly AppDbContext _context;
 
@@ -34,7 +18,6 @@ namespace VMS.WebApp.Controllers
             _context = context;
         }
 
-<<<<<<< HEAD
         // GET: api/bookings
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
@@ -118,148 +101,6 @@ namespace VMS.WebApp.Controllers
         private async Task<bool> BookingExists(int id)
         {
             return await _context.Bookings.AnyAsync(b => b.BookingID == id);
-=======
-        // GET: Bookings
-        public async Task<IActionResult> Index()
-        {
-            var appDbContext = _context.Bookings.Include(b => b.Event);
-            return View(await appDbContext.ToListAsync());
-        }
-
-        // GET: Bookings/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings
-                .Include(b => b.Event)
-                .FirstOrDefaultAsync(m => m.BookingID == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
-
-        // GET: Bookings/Create
-        public IActionResult Create()
-        {
-            ViewData["EventID"] = new SelectList(_context.Events, "EventID", "EventID");
-            return View();
-        }
-
-        // POST: Bookings/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BookingID,UserID,EventID,BookingDate,TotalPrice,Status")] Booking booking)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(booking);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["EventID"] = new SelectList(_context.Events, "EventID", "EventID", booking.EventID);
-            return View(booking);
-        }
-
-        // GET: Bookings/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings.FindAsync(id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-            ViewData["EventID"] = new SelectList(_context.Events, "EventID", "EventID", booking.EventID);
-            return View(booking);
-        }
-
-        // POST: Bookings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("BookingID,UserID,EventID,BookingDate,TotalPrice,Status")] Booking booking)
-        {
-            if (id != booking.BookingID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(booking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookingExists(booking.BookingID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["EventID"] = new SelectList(_context.Events, "EventID", "EventID", booking.EventID);
-            return View(booking);
-        }
-
-        // GET: Bookings/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Bookings
-                .Include(b => b.Event)
-                .FirstOrDefaultAsync(m => m.BookingID == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
-
-        // POST: Bookings/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var booking = await _context.Bookings.FindAsync(id);
-            if (booking != null)
-            {
-                _context.Bookings.Remove(booking);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool BookingExists(int id)
-        {
-            return _context.Bookings.Any(e => e.BookingID == id);
->>>>>>> aaa622ac58bfdcc7921fbe7a32ff5642379546f7
         }
     }
 }
