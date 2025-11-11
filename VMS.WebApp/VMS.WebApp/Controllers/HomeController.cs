476a4c22b2453541a96f9e81ee52fc417628643a
Jove Tondo <jove.tondo@gmail.com>
2025-11-12 00:45:26 +1300

    Added sample Park Info layout and Events page

--- VMS.WebApp/VMS.WebApp/Controllers/HomeController.cs
+++ VMS.WebApp/VMS.WebApp/Controllers/HomeController.cs
@@ -52,6 +52,11 @@ namespace VMS.WebApp.Controllers
             return View();
         }
 
+        public IActionResult Events()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
