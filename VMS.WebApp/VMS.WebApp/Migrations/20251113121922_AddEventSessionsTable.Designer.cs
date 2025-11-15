5913ec99fdc05c0e27d38425a3356817573cc7cd
Jove Tondo <jove.tondo@gmail.com>
2025-11-16 00:03:45 +1300

    Added Login/Register/Account page

--- VMS.WebApp/VMS.WebApp/Migrations/20251113121922_AddEventSessionsTable.Designer.cs
+++ VMS.WebApp/VMS.WebApp/Migrations/20251113121922_AddEventSessionsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VMS.WebApp.Data;
@@ -11,9 +12,11 @@ using VMS.WebApp.Data;
 namespace VMS.WebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113121922_AddEventSessionsTable")]
+    partial class AddEventSessionsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VMS.WebApp/VMS.WebApp/Models/EventSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VMS.WebApp.Models
+{
+    public class EventSession
+    {
+        public int SessionID { get; set; }
+
+        public int EventID { get; set; }
+        public Event Event { get; set; } = null!;
+
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+
+        public int Capacity { get; set; }
+        public decimal Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
